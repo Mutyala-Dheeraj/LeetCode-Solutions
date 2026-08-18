4fdcbb3ae5d567bb1581d3acd2b3668e6b3238bc
Mutyala-Dheeraj <dheerajmutyala21@gmail.com>
2026-08-18 22:06:19 +0530

    Time: 1258 ms (5.30%) | Memory: 12.5 MB (51.45%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -5,9 +5,15 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        num = sorted(nums1 + nums2)
-        n = len(num)
-        if n%2 == 0:
-            return float(num[n//2 - 1] + num[n//2 ])/2.0
+        nums = nums1 + nums2
+        n = len(nums)
+        n = len(nums)
+        for i in range (0,n-1):
+            for j in range (i+1,n):
+                if nums[i] > nums[j]:
+                    nums[i],nums[j] = nums[j],nums[i]
+        if n%2==0:
+            return (nums[(n-1)//2]+nums[n//2])/2.0
         else:
-            return float(num[n//2])
+            return nums[n//2]
+
